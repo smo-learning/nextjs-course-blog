0fb45ca63db45759aa1acdfe86dabc99de997540
smotastic <nilssmol@gmail.com>
2022-01-01 18:03:09 +0100

    CustomRenderers

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,8 +2,8 @@ import PostHeader from "./post-header";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import classes from './post-content.module.css';
 import Image from "next/image";
-
-
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 export default function PostContent({ post }) {
     const imagePath = `/images/posts/${post.slug}/${post.image}`;
@@ -11,6 +11,10 @@ export default function PostContent({ post }) {
     const customRenderers = {
         img(image) {
             return <Image src={`/images/posts/${post.slug}/${image.src}`} alt={image.alt} width={600} height={300} />
+        },
+        code(code) {
+            const { language, children } = code;
+            return <SyntaxHighlighter style={atomDark} language={language} children={children} />
         }
     }
 
